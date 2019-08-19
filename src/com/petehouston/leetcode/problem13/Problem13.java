a24aaaf9a22de8f03034b1647e97a94a99ac360c
Pete Houston <contact@petehouston.com>
2019-08-19 00:22:12 -0500

    Improve problem 13

--- src/com/petehouston/leetcode/problem13/Problem13.java
+++ src/com/petehouston/leetcode/problem13/Problem13.java
@@ -3,6 +3,7 @@ package com.petehouston.leetcode.problem13;
 public class Problem13 {
     public int romanToInt(String s) {
         if (s.length() < 1) return 0;
+        char[] data = s.toCharArray()
 
         int total = 0;
         for (int i = 0; i < s.length(); i++) {
@@ -25,6 +26,47 @@ public class Problem13 {
                 total += 100;
                 if (i + 1 < s.length()) {
                     if (s.charAt(i + 1) == 'D' || s.charAt(i + 1) == 'M') total -= 200;
+                }
+            }
+            else if (c == 'D') total += 500;
+            else if (c == 'M') total += 1000;
+        }
+
+        return total;
+    }
+
+    /**
+     * Improvement:
+     *      s.length() : is costly
+     *      s.charAt() : is also costly
+     *
+     *  Instead, breakdown string into char array and deal with array.
+     */
+    public int romanToInt_improved(String s) {
+        if (s.length() < 1) return 0;
+        char[] data = s.toCharArray();
+
+        int total = 0;
+        for (int i = 0; i < data.length; i++) {
+            char c = data[i];
+            if (c == 'I') {
+                total += 1;
+                if (i + 1 < data.length) {
+                    if (data[i+1] == 'V' || data[i+1] == 'X') total -= 2;
+                }
+            }
+            else if (c == 'V') total += 5;
+            else if (c == 'X') {
+                total += 10;
+                if (i + 1 < data.length) {
+                    if (data[i+1] == 'L' || data[i+1] == 'C') total -= 20;
+                }
+            }
+            else if (c == 'L') total += 50;
+            else if (c == 'C') {
+                total += 100;
+                if (i + 1 < data.length) {
+                    if (data[i+1] == 'D' || data[i+1] == 'M') total -= 200;
                 }
             }
             else if (c == 'D') total += 500;
